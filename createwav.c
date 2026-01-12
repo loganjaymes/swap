68db17454894be4e7cc83fe5bfcdc67a71bf7db8
Logan Bjork <loganbjork@ufl.edu>
2026-01-11 20:31:08 -0500

    fixed stupadadh bug (types were wrong for bits/bytes)

--- createwav.c
+++ createwav.c
@@ -1,10 +1,12 @@
 #include "stdio.h"
 #include "string.h"
+#include "math.h"
+#include "stdint.h"
 
 // create wav header: https://docs.fileformat.com/audio/wav/ 
 struct wav_header {
 	char riff[4];
-	int32_t size;
+	int32_t file_size;
 	char wave[4];
 	char fmt[4];
 	int32_t chunk_size;
@@ -12,16 +14,48 @@ struct wav_header {
 	int16_t num_channels;
 	int32_t sample_rate;
 	int32_t bytes_sec;
-	int32_t bytes_sample;
-	int32_t bits_sample;
+	int16_t bytes_sample;
+	int16_t bits_sample;
 	char data[4];
 	int32_t data_len;
-}
-
+};
 
 int main() {
+	const int sample_rate = 44100;
+
 	struct wav_header wh;
 
 	strncpy(wh.riff, "RIFF", 4);
-	
+	strncpy(wh.wave, "WAVE", 4);
+	strncpy(wh.fmt, "fmt ", 4);
+	strncpy(wh.data, "data", 4);
+
+	wh.chunk_size = 16;
+	wh.format_tag = 1;
+	wh.num_channels = 1;
+	wh.sample_rate = sample_rate;
+	wh.bits_sample = 16;
+	wh.bytes_sample = (wh.bits_sample * wh.num_channels) / 8;
+	wh.bytes_sec = wh.sample_rate * wh.bytes_sample;
+
+	const int dur_sec = 8; // audio len
+	const int buf_size = sample_rate * dur_sec;
+
+	wh.data_len = buf_size * wh.bytes_sample;
+	wh.file_size = wh.data_len + 36;
+
+	// write notes to wav
+	short int buf[buf_size] = {};
+
+	for (int i = 0; i < buf_size; ++i) {
+		buf[i] = (short int)(1000 * sin((2 * M_PI * 440.0 * i) / sample_rate));
+	}
+
+	// write to file
+	FILE* fp = fopen("test.wav", "wb");
+	fwrite(&wh, 1, sizeof(wh), fp);
+	fwrite(buf, 2, buf_size, fp);
+	fclose(fp);
+
+	return 0;
 }
